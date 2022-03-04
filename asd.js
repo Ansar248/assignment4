1170020e8ce7882453a6ca50b855b90063592290
Ansar248 <96940669+Ansar248@users.noreply.github.com>
2022-03-04 20:13:57 +0600

    Add files via upload

---
+++ asd.js
@@ -0,0 +1,32 @@
+function Creators() {
+	alert('Ansar +77761050756 \nAdil +79684906367 ');
+	var sample = document.getElementById("foobar");
+sample.play();
+}
+function showFacts() {
+	document.getElementById('facts').innerHTML="He tried to buy Marvel Comics.";
+	var sample = document.getElementById("foobar");
+sample.play();
+}
+function hideFacts() {
+	document.getElementById('facts').innerHTML=" ";
+	var sample = document.getElementById("foobar");
+sample.play();
+}
+function ShowJ(){
+	document.getElementById('Jack').innerHTML="Michael Joseph Jackson";
+	var sample = document.getElementById("foobar");
+sample.play();
+}
+function HideJ(){
+	document.getElementById('Jack').innerHTML="Michael Jackson";	
+	var sample = document.getElementById("foobar");
+sample.play();
+}
+
+
+
+
+
+
+  
